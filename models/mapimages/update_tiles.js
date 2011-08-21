b5ac7f1e302d439b674b002aef494d6205f6b260
Dave Edelhart <dave@wonderlandlabs.com>
2011-08-21 13:27:34 -0700

    Streaming data into mongo directly

--- models/mapimages/update_tiles.js
+++ models/mapimages/update_tiles.js
@@ -1,6 +1,9 @@
 var mm = require(MVC_MODELS);
-//var Pipe = require('util/pipe');
-var Gate = require('util/gate');
+var pm = require('path');
+var fs = require('fs');
+var Pipe = require('util/pipe');
+var bin = require('util/binary');
+//var Gate = require('util/gate');
 /**
  * mapimage: update_tiles
  * updates all the tiles for  a single image id
@@ -8,128 +11,93 @@ var Gate = require('util/gate');
  * @param scale
  * @param callback
  */
-module.exports = function (image, scale, callback) {
-    var self = this;
-    var start_time = new Date().getTime();
-    mm.model('mapimage_tile', function(err, mit_model) {
 
-        function _import_data(err, image_data) {
-            var tile_est = image.rows * image.cols / (scale * scale);
-            var image_done_count = 0;
-            var tile_cursor;
-            var statics;
-            //          var pipe_2;
+function _stub() {
+}
+/* ************************ save_int_row_slice ***************** */
 
-            console.log('estimated ', tile_est, ', tiles');
-/*
-//            tile_cursor = mit_model.find({image: image._id}, {heights: 0});
-            tile_cursor.count(function(err, c) {
-                tile_est = c;
-            });*/
-            var image_load_time = new Date().getTime();
-
-         //   var gate = new Gate(callback);
-         //   var gcb = gate.task_done_callback(false);
-            console.log('image rows: ', image.rows, 'image.cols:', image.cols);
-            console.log('image_data.rows: ', image_data.length, 'image_data.cols', image_data[0].length);
-            var i_limit = image.rows - scale;
-            var j_limit = image.cols - scale;
-            console.log('i_limit:', i_limit, 'j_limit:', j_limit);
-            
-            for (var i = 0; i < i_limit; i += scale)
-                for (var j = 0; j < j_limit; j += scale) {
-                  //  if (i > i_limit) continue;
-                 //   if (j > j_limit) continue;
-                    var heights = _chunk(image_data, i, i + scale, j, j + scale);
-                    var query = {min_image_i: i, min_image_j: j, image: image._id};
-                    var set_heights = {"$set": {heights: heights}};
-                    mit_model.update(query, _stub, set_heights);
-                  //  gate.task_start();
-                    ++image_done_count;
-                    if (!(image_done_count % 100)) {
-                        _report_times(start_time, image_load_time, tile_est, image_done_count);
-                    }
-                }
-            callback();
-            /*
-             //        statics = {tile_cursor: tile_cursor, mit_model: mit_model};
-             //      var no_tile_count = 0;
-
-             function _update_tile(param, static_params, a2_done, p2_done) {
-             tile_cursor.next(function(err, tile) {
-             if (!tile) {
-             if (++ no_tile_count > 4) {
-             console.log('no tile - done with image ', image._id);
-             p2_done();
-             }
-             return;
-             }
-             // if (!(image_done_count % 10)) {
-             //      console.log(__filename, 'updating tile ', tile._id);
-             //   }
-             //  mit_model.update_tile(tile, a2_done, image_data);
-
-             var height_rows = [];
-
-             for (var i = tile.min_image_i; i < tile.max_image_i; ++i) {
-             if (i >= image_data.length) {
-             throw new Error('attempting to read row ' + i + ', past image length ' + image_data.length);
-             }
-             var row = image_data[i];
-             // console.log('row: ', row.length, '; ', tile.min_image_j, 'x', tile.max_image_j);
-             height_rows.push(row.slice(tile.min_image_j, tile.max_image_j));
-             }
-
-             tile.heights = height_rows;
-             mit_model.put(tile, a2_done);
-             ++image_done_count;
-             if (!(image_done_count % 100)) {
-             _report_times(start_time, image_load_time, tile_est, image_done_count);
-             }
-             })
-             }
-
-             pipe_2 = new Pipe(callback, _update_tile, 200, false, statics);
-             pipe_2.start();
-
-             */
+function save_int_row_slice(image, i_offset, data, scale, mit_model) {
+    for (var j = 0; j < image.cols; j += scale) {
+        var heights = [];
+        for (var i = 0; i < data.length; ++i) {
+            heights[i] = data[i].slice(j, j + scale);
         }
 
-        console.log(__filename, ': image: ', image);
-        self.import_image_data(image, _import_data);
+        var query = {min_image_i: i_offset, min_image_j: j, image: image._id};
+        mit_model.update({"$set": {heights: heights}}, null, query);
+    }
+}
+
+/* ************************ import data ************************ */
+
+module.exports = function(image, scale, callback) {
+
+    mm.model('mapimage_tile', function(err, mit_model) {
+        console.log(__filename, ': input ', image._id);
+
+        var path = image.image_file,
+            rows = image.rows,
+            cols = image.cols,
+            i_offset = 0;
+
+        console.log(__filename, ': reading ', path, 'r', rows, 'c', cols);
+        if (!pm.existsSync(path)) {
+            throw new Error(__filename + ': cannot find ' + path);
+        }
+
+        var file_info = fs.statSync(path);
+        console.log(file_info);
+
+        var read_config = {
+            bufferSize: 64 * 1024
+        };
+
+        var ints = [];
+        var int_rows = [];
+
+        var handle = fs.createReadStream(path, read_config);
+        var read_size = 0;
+        var last_percent = 0;
+        var data_count = 0;
+        var expected = rows * cols;
+
+        handle.on('data', function(data) {
+            ++data_count;
+            var new_data = bin.int_array(data);
+            ints = ints.concat(new_data);
+            while (ints.length >= cols) {
+                int_rows.push(ints.slice(0, cols));
+                ints = ints.slice(cols);
+
+                if (int_rows.length > scale) {
+
+                    var int_row_slice = int_rows.slice(0, scale + 1);
+                    int_rows = int_rows.slice(scale);
+
+                    save_int_row_slice(image, i_offset, int_row_slice, scale, mit_model);
+                    i_offset += scale;
+                }
+            }
+            read_size += new_data.length;
+            var percent = read_size * 100 / expected;
+            if ((percent - last_percent) >= 10) {
+                console.log(parseInt(percent), '% read ', read_size, ' of ', expected, ' ints');
+                last_percent = percent;
+            }
+        });
+
+        handle.on('end', function() {
+            if (ints.length) {
+                int_rows.push(ints);
+            }
+            save_int_row_slice(image, i_offset, int_rows, scale, mit_model);
+            console.log(__filename, ' >>>>>>>>>>>>>>>>>>>>>>>>>> DONE! ');
+            callback();
+        })
 
     });
-
 }
 
-function _stub(){}
-
-function _chunk(image_data, i_start, i_end, j_start, j_end) {
-    var height_rows = [];
-    if (i_start >= image_data.length){
-        throw new Error('out of range i_start: ' + i_start);
-    }
-
-    if (j_start >= image_data[0].length){
-        throw new Error('out of range j_start: ' + j_start);
-
-    }
-
-    i_end = Math.min(i_end, image_data.length - 1);
-    j_end = Math.min(j_end, image_data[0].length - 1);
-  //  console.log('chunking i: ', i_start, i_end,'j: ', j_start, j_end);
-    
-    for (var i = i_start; i < i_end; ++i) {
-        if (i >= image_data.length) {
-            throw new Error('attempting to read row ' + i + ', past image length ' + image_data.length);
-        }
-        var row = image_data[i];
-        // console.log('row: ', row.length, '; ', tile.min_image_j, 'x', tile.max_image_j);
-        height_rows.push(row.slice(j_start, j_end));
-    }
-
-    return height_rows;
-}
 
 function texp(mil) {
     mil /= 1000;
@@ -144,8 +112,10 @@ function texp(mil) {
         return secs + ' seconds';
     }
 }
+
 var last_rem_est = 0;
 var last_percent = 0;
+
 function _report_times(start_time, il_time, total_tiles, done_tiles) {
     var now = new Date().getTime();
     var tile_elapse = now - il_time;
